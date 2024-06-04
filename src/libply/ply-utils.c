64379c1100a0177f52e130601d44a7ffe0fdedf1
Hans de Goede <hdegoede@redhat.com>
2024-06-04 21:09:00 +0200

    ply-utils: Add ply_string_has_suffix () helper function
    Add a ply_string_has_suffix () helper function to match the existing
    ply_string_has_prefix () helper function.

--- src/libply/ply-utils.c
+++ src/libply/ply-utils.c
@@ -473,6 +473,24 @@ ply_string_has_prefix (const char *str,
         return strncmp (str, prefix, strlen (prefix)) == 0;
 }
 
+bool
+ply_string_has_suffix (const char *str,
+                       const char *suffix)
+{
+        size_t str_len, suffix_len;
+
+        if (str == NULL || suffix == NULL)
+                return false;
+
+        str_len = strlen (str);
+        suffix_len = strlen (suffix);
+
+        if (suffix_len > str_len)
+                return false;
+
+        return strcmp (str + (str_len - suffix_len), suffix) == 0;
+}
+
 double
 ply_get_timestamp (void)
 {
