7844161fc567954b409ae89f801652d45696e677
Ray Strode <rstrode@redhat.com>
2026-07-22 08:28:29 -0400

    ply-utils: Use private Secure Boot reader
    Secure Boot detection reads and interprets the firmware variable beside its
    process-wide cache.
    
    The fixed path prevents direct coverage of malformed variable sizes.
    
    This commit delegates file validation to the private reader while retaining
    the existing cached public result.
    
    The next commit will cover firmware-variable contents with fixtures.

--- src/libply/ply-utils.c
+++ src/libply/ply-utils.c
@@ -52,6 +52,7 @@
 #include <dlfcn.h>
 
 #include "ply-logger.h"
+#include "ply-secure-boot-private.h"
 
 #ifndef PLY_OPEN_FILE_DESCRIPTORS_DIR
 #define PLY_OPEN_FILE_DESCRIPTORS_DIR "/proc/self/fd"
@@ -80,7 +81,6 @@
 #define EFI_VARIABLES_PATH "/sys/firmware/efi/efivars/"
 #define EFI_GLOBAL_VARIABLES_GUID "8be4df61-93ca-11d2-aa0d-00e098032b8c"
 #define SECURE_BOOT_GLOBAL_VARIABLES_FILE EFI_VARIABLES_PATH "SecureBoot-" EFI_GLOBAL_VARIABLES_GUID
-#define IS_SECURE_BOOT_ENABLED(sb_config) ((sb_config) == 0x1)
 
 static int errno_stack[PLY_ERRNO_STACK_SIZE];
 static int errno_stack_position = 0;
@@ -1384,32 +1384,6 @@ double ply_strtod (const char *str)
         return ret;
 }
 
-static bool
-check_secure_boot_settings (const char *filename)
-{
-        int fd;
-        int len;
-        uint8_t buf[5];
-
-        fd = open (filename, O_RDONLY);
-        if (fd < 0)
-                return false;
-
-        len = read (fd, buf, 5);
-        close (fd);
-
-        /* /sys/firmware/efi/efivars/SecureBoot-8be4df61-93ca-11d2-aa0d-00e098032b8c
-         * is in a binary format. The file is exactly 5 bytes long and the last byte
-         * is the secure boot configuration. If it is 0x1, the secure boot is
-         * enabled.
-         */
-        if (len == 5)
-                if (IS_SECURE_BOOT_ENABLED (buf[4]))
-                        return true;
-
-        return false;
-}
-
 bool
 ply_is_secure_boot_enabled (void)
 {
@@ -1418,7 +1392,7 @@ ply_is_secure_boot_enabled (void)
         if (is_secure_boot_enabled != -1)
                 return is_secure_boot_enabled;
 
-        if (check_secure_boot_settings (SECURE_BOOT_GLOBAL_VARIABLES_FILE))
+        if (ply_secure_boot_enabled_at_path (SECURE_BOOT_GLOBAL_VARIABLES_FILE))
                 is_secure_boot_enabled = true;
         else
                 is_secure_boot_enabled = false;
