2b08657fd9b7a52b2272bd0c7d9b05cae74ef494
Ray Strode <rstrode@redhat.com>
2026-07-22 07:46:48 -0400

    tests: Cover installed theme listing
    The default-theme utility discovers themes from installed descriptor files
    beneath the configured data directory.
    
    No functional case verifies discovery without depending on themes installed
    on the build host.
    
    This commit registers a temporary installation fixture and checks the names
    returned by the list operation.
    
    The next commit will cover selecting a named theme.

---
+++ tests/test-set-default-theme.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Copyright (C) 2026 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+
+set -eu
+
+theme_chooser=$1
+test_directory=$(mktemp -d /tmp/plymouth-set-theme-XXXXXX)
+trap 'rm -rf "$test_directory"' EXIT
+
+data_directory=$test_directory/share
+config_directory=$test_directory/etc
+policy_directory=$test_directory/policy
+plugin_directory=$test_directory/plugins
+libexec_directory=$test_directory/libexec
+
+mkdir -p "$data_directory/plymouth/themes/alpha"
+mkdir -p "$data_directory/plymouth/themes/zeta"
+printf '%s\n' '[Plymouth Theme]' 'ModuleName=script' > \
+        "$data_directory/plymouth/themes/alpha/alpha.plymouth"
+printf '%s\n' '[Plymouth Theme]' 'ModuleName=script' > \
+        "$data_directory/plymouth/themes/zeta/zeta.plymouth"
+
+run_theme_chooser()
+{
+        PLYMOUTH_DATADIR=$data_directory \
+        PLYMOUTH_CONFDIR=$config_directory \
+        PLYMOUTH_POLICYDIR=$policy_directory \
+        PLYMOUTH_PLUGIN_PATH=$plugin_directory/ \
+        PLYMOUTH_LIBEXECDIR=$libexec_directory \
+                "$theme_chooser" "$@"
+}
+
+echo 'TAP version 13'
+echo '1..1'
+
+themes=$(run_theme_chooser --list)
+if [[ $themes == $'alpha\nzeta' ]]; then
+        echo 'ok 1 - list installed themes'
+else
+        echo "not ok 1 - list installed themes: got '$themes'"
+        exit 1
+fi
