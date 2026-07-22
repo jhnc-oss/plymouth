daa0c0dda9c46385c6f04b0a589ff62c4c34448a
Ray Strode <rstrode@redhat.com>
2026-07-22 07:51:06 -0400

    tests: Cover generated initrd archive contents
    The initrd generator populates a temporary tree and emits it as a
    compressed newc archive.
    
    Population tests do not verify that the wrapper invokes its helper or
    preserves copied paths in the final image.
    
    This commit supplies a recording populate command and inspects the
    decompressed archive for configuration and client files.
    
    The next commit will cover removal of shared libraries supplied by the host
    initrd.

---
+++ tests/test-generate-initrd.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Copyright (C) 2026 Red Hat, Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+
+set -eu
+
+generate_initrd=$1
+test_directory=$(mktemp -d /tmp/plymouth-generate-initrd-XXXXXX)
+trap 'rm -rf "$test_directory"' EXIT
+unset DESTDIR
+
+populate_command=$test_directory/populate-initrd
+image_file=$test_directory/initrd-plymouth.img
+
+printf '%s\n' \
+        '#!/bin/bash' \
+        'set -eu' \
+        'while [[ $# -gt 0 ]]; do' \
+        '        case $1 in' \
+        '                -t|--targetdir)' \
+        '                        shift' \
+        '                        target_directory=$1' \
+        '                        ;;' \
+        '        esac' \
+        '        shift' \
+        'done' \
+        'mkdir -p "$target_directory/etc" "$target_directory/usr/bin"' \
+        'printf "%s\n" "[Daemon]" "Theme=alpha" > "$target_directory/etc/plymouthd.conf"' \
+        'printf "%s\n" client > "$target_directory/usr/bin/plymouth"' > \
+        "$populate_command"
+chmod +x "$populate_command"
+
+echo 'TAP version 13'
+echo '1..1'
+
+PLYMOUTH_POPULATE_INITRD=$populate_command \
+PLYMOUTH_IMAGE_FILE=$image_file \
+        "$generate_initrd"
+
+archive_contents=$(gzip --decompress --stdout "$image_file" |
+        cpio --list 2>/dev/null)
+
+if grep --quiet --line-regexp 'etc/plymouthd.conf' <<< "$archive_contents" &&
+        grep --quiet --line-regexp 'usr/bin/plymouth' <<< "$archive_contents"; then
+        echo 'ok 1 - generated cpio contents'
+else
+        echo 'not ok 1 - generated cpio contents'
+        exit 1
+fi
